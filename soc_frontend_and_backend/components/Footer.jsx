a0d44dabb02cbaeb6afc578338ebb32c9ec43111
Amit-Rohila33 <210010008@iitb.ac.in>
2023-05-26 12:12:46 +0530

    project created and initiation of components created

---
+++ soc_frontend_and_backend/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      This is the <b>Footer</b>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ soc_frontend_and_backend/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      This is the <b>Header</b>
+    </div>
+  );
+};
+
+export default Header;
